63acf32c78abbe7bef814ab9c5c7525d15238c71
AKASHPRABHU <108968305+PRABHUAKASH@users.noreply.github.com>
2022-07-21 09:16:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+// circle 
+// class circle{
+//     constructor(radius,color){
+//         this.radius=radius;
+//         this.color=color;
+//     }   
+// }
+// var a=new circle(1.0,"red");
+// console.log(a)
+
+
+
+
+// radius
+// class circle{
+//     constructor(radius,color){
+//         this.radius=radius;
+//         this.color=color;
+// }
+//    getradius( ){
+//     return "the circle radius is" + "this.radius";
+//     }   
+//     set changeradius(modifie name){
+//         this.radius=modifie name;
+//     }
+// }
+// var a=new circle (1.0,"red");
+// a.changeradius=2.0;
+// console.log(a.radius);
+
+
+
+// color 
+// class circle{
+//     constructor(radius,color){
+//         this.radius=radius;
+//         this.color=color;
+// }
+//    getcolor( ){
+//     return "the circle color is" + "this.color";
+//     }   
+//     set changecolor(modifie color){
+//         this.color=modifie color;
+//     }
+// }
+// var a=new circle (1.0,"red");
+// a.changecolors="yellow";
+// console.log(a.color);
+
+
+// AREA 
+// class circle{
+//     constructor(radius,color){
+//         this.radius=radius;
+//         this.color=color;
+//     }   
+//     getarea(){
+//         return pi*this.radius*this.radius;
+//     }
+// }
+
+// var a = new circle(1.0,"red");
+// console.log(a);
+
+// CIRCUMFRENCE
+// class circle{
+//     constructor(radius,color){
+//         this.radius=radius;
+//         this.color=color;
+//     }   
+//     getcircumfrence(){
+//         return 2*pi*this.radius;
+//     }
+// }
+
+// var a = new circle(1.0,"red");
+// console.log(a);  
+
+
+
+
